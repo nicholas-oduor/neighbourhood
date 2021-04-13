8c071e4c42435a3943a73810b8ec69d55cdce025
nicholas-oduor <oduor5742@gmail.com>
2021-04-13 08:56:17 +0300

    modified class LoginAPI

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import viewsets,permissions
+from .models import *
+from .serializer import *
+from rest_framework import status
+from .permissions import IsAdminOrReadOnly
+from rest_framework.decorators import action, permission_classes as permission_decorator
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from django.contrib.auth import login
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from knox.views import LoginView as KnoxLoginView
+class LoginAPI(KnoxLoginView):
+    permission_classes = (permissions.AllowAny,)
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(LoginAPI, self).post(request, format=None)
+
+
+def home(request):
+    hood = Neighbourhood.objects.all()
+    business = Business.objects.all()
+    posts = Post.objects.all()
+    # print("Results..", posts)
+    heading = "Welcome to Neighborhood application"
+    return render(request, "home.html", {"hoods":hood, "business":business,"posts":posts, "heading":heading})
