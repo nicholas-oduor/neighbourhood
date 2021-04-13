66f768087617c18b3c388b41b08c31f72a9ea544
nicholas-oduor <oduor5742@gmail.com>
2021-04-13 09:55:39 +0300

    updated urls.py file in neighbourhood project

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -14,8 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.contrib.auth import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('hood.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path("accounts/logout", views.logout_then_login, name= "logout"),
+    path('tinymce/', include('tinymce.urls')),
+    path('api-token-auth/', obtain_auth_token)
 ]
+
+admin.site.index_title="Neighborhood"
+admin.site.site_header="Neighborhood Admin"
+admin.site.site_title="Neighborhood"
