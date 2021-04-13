86b710f91dd1fa1ff1c8ccf2366869bd3ea8ac01
nicholas-oduor <oduor5742@gmail.com>
2021-04-13 08:05:59 +0300

    updated admin.py

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Neighbourhood)
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Post)
 
 # Register your models here.
